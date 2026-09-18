613705b11d3f875edac283b2cd0d4bf33820fff7
Thomas Judy <tommyjudy429@gmail.com>
2026-09-18 17:15:52 -0500

    vs update

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D body;
+
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        body.angularVelocity = new Vector3(Input.GetAxis("Horizontal"),0,0);
     }
+
 }
